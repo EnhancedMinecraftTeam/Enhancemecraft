7b1d7b3edd1dc9d4cb461f8a59f22595795beec2
Marc Singer <nextcode@mecanized.net>
2013-08-14 15:43:12 +0200

    Pre Minor Update 0.3.0 (#2)

--- common/com/nexized/cross/lib/libInfo.java
+++ common/com/nexized/cross/lib/libInfo.java
@@ -4,6 +4,7 @@ public class libInfo {
 
 	// @Lib Information
 	public final static String ID = "crosslib";
+	public final static int updatedID = 1;
 	
 	/*
 	 * This void return a resource compatible string
