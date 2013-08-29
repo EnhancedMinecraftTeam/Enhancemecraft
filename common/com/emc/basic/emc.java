5cfbf43b591c81195f6fdb5737954114fdd79b6a
Marc Singer <nextcode@mecanized.net>
2013-08-29 20:39:42 +0200

    Complete rework (#1)

---
+++ common/com/emc/basic/emc.java
@@ -0,0 +1,18 @@
+package com.emc.basic;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.network.NetworkMod;
+
+/*
+ * EnhanceMeCraft Basic Modification
+ * @Author: nextized
+ * @Last changed: 2013-08-30
+ * Licensed under nextized cross license - see license.txt for more information
+ */
+
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+@Mod(modid = emclib.ID, name = emclib.NAME, version = emclib.VERSION)
+
+public class emc {
+
+}
