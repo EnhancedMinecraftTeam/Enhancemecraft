804abec094b01676850e5f1e23c418a2e920e4b6
Marc Singer <nextcode@mecanized.net>
2013-09-12 20:22:21 +0200

    Minor Update 0.3.4.5

--- common/com/emc/world/biome/biomeManager.java
+++ common/com/emc/world/biome/biomeManager.java
@@ -1,5 +1,12 @@
 package com.emc.world.biome;
 
+/*
+ * EnhanceMeCraft Basic Modification
+ * @Author: nextized
+ * @Last changed: 2013-09-12
+ * Licensed under nextized cross license - see license.txt for more information
+ */
+
 import com.emc.conf.idManager;
 
 public class biomeManager {
